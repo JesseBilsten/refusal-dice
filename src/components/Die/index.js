ba2462f753f00452265d985633a19866f4f4f2eb
Jesse Bilsten <jesse@bilsten.net>
2019-03-25 08:10:12 -0700

    Add die size

--- src/components/Die/index.js
+++ src/components/Die/index.js
@@ -5,6 +5,7 @@ class Die extends Component {
   render() {
     const props = this.props
     const dieSize = 2
+		if (props.size) { dieSize = props.size }
 
     return (
       <div className="die pips" data-content={props.number}>
