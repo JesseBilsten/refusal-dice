ff5a20cfbb9f2d4982871cb275c568b8cb0dd588
Jesse Bilsten <jesse@bilsten.net>
2019-03-25 07:49:12 -0700

    Another attempt to use bootstrap better

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,38 +6,51 @@
 
 const webpack = require('webpack')
 
-exports.onCreateWebpackConfig = ({
-  stage,
-  rules,
-  loaders,
-  plugins,
-  actions,
-}) => {
-  if (stage === 'build-html') {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: require.resolve('bootstrap'),
-            use: loaders.null(),
-          },
-          {
-            test: require.resolve('jquery'),
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
-  actions.setWebpackConfig({
+exports.onCreateWebpackConfig = ({ actions, stage, loaders }) => {
+  const config = {
     plugins: [
       new webpack.ProvidePlugin({
-        $: 'jquery',
         jQuery: 'jquery',
-        'window.jQuery': 'jquery',
-        Popper: 'popper.js',
-        Bootstrap: 'bootstrap.js',
+        $: 'jquery',
+        jquery: 'jquery',
       }),
     ],
-  })
+  }
+  if (stage === 'build-html') {
+    config.module = {
+      rules: [
+        {
+          test: require.resolve('bootstrap'),
+          use: loaders.null(),
+        },
+        {
+          test: require.resolve('bootstrap-table'),
+          use: loaders.null(),
+        },
+        {
+          test: require.resolve(
+            'bootstrap-table/dist/extensions/mobile/bootstrap-table-mobile'
+          ),
+          use: loaders.null(),
+        },
+        {
+          test: require.resolve(
+            'bootstrap-table/dist/extensions/sticky-header/bootstrap-table-sticky-header'
+          ),
+          use: loaders.null(),
+        },
+        {
+          test: require.resolve(
+            'bootstrap-table/dist/extensions/cookie/bootstrap-table-cookie'
+          ),
+          use: loaders.null(),
+        },
+        {
+          test: require.resolve('jquery'),
+          use: loaders.null(),
+        },
+      ],
+    }
+  }
+  actions.setWebpackConfig(config)
 }
