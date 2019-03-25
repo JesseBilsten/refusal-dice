4d48c4cf04a13149e2e8b0c3d74d682e2d18796f
Jesse Bilsten <jesse@bilsten.net>
2019-03-25 08:10:32 -0700

    SEO links page

---
+++ src/pages/links.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const LinksPage = () => (
+  <Layout>
+    <a href="https://eightygames.wordpress.com/tag/san-francisco-bar-dice/">
+      Variation
+    </a>
+  </Layout>
+)
+
+export default LinksPage
