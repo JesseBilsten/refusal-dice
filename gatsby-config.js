b4374a4377a7ee223026fcbd06c9b298c3e02617
Jesse Bilsten <jesse@bilsten.net>
2026-01-14 12:38:18 -0800

    chore(deps): upgrade Gatsby/plugins to v5/v6, replace node-sass with sass, add .nvmrc

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,19 +14,23 @@ module.exports = {
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/images/game-die.png', // This path is relative to the root of the site.
-			},
+        },
+        },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
         path: `${__dirname}/src/`,
-				},
-		},
-		{
-		resolve: `gatsby-plugin-sass`,
-    options: {
-      precision: 8,
-			},
-		},
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        sassOptions: {
+          precision: 8,
+        },
+      },
+    },
     'gatsby-plugin-offline',
   ],
 }
